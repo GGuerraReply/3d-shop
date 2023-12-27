028b2cbbee8f20cf6d1fe45e64c23fc6c1b1c09d
GGuerraReply <g.guerra@reply.it>
2023-12-27 11:22:28 +0100

    fix on api side

--- api/index.js
+++ api/index.js
@@ -6,10 +6,14 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limig: "50mb" }));
+app.use(express.json({ limit: "50mb" }));
 
-app.get("/", (req, res) => {
+const router = express.Router();
+
+router.get("/", (req, res) => {
   res.status(200).send("Hello from DALL-E");
 });
+
+app.use('/api', router);
 
 app.listen(80, () => console.log(`Server is listening on port ${process.env.PORT}`));
