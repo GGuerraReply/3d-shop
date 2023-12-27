736e82106983b944549ec6678b503a27357dd020
GGuerraReply <g.guerra@reply.it>
2023-12-27 13:03:24 +0100

    test

--- server/api/index.js
+++ server/api/index.js
@@ -1,24 +1,5 @@
-import express from "express";
-import * as dotenv from 'dotenv';
-import cors from 'cors';
-import { createAzureFunctionHandler } from 'azure-function-express';
-
-dotenv.config();
-
-const app = express();
-app.use(cors());
-app.use(express.json({ limit: "50mb" }));
-
-app.get("/", (req, res) => {
-  res.status(200).send("Hello from DALL-E");
-});
-
-app.get("/test", (req, res) => {
-  res.status(200).send("test");
-});
-
-if (process.env.IS_LOCAL === "true") {
-  app.listen(80, () => console.log(`Server is listening on port 80`));
-}
-
-export default createAzureFunctionHandler(app);
+module.exports = async function (context, req) {
+  context.res.json({
+      text: "Hello from the API"
+  });
+};
