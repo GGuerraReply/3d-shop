68e5e9b79da9ad8db59457f7dbf6599e994957cc
GGuerraReply <g.guerra@reply.it>
2023-12-28 02:56:25 +0100

    aggiunto expressjs

--- api/hello/index.js
+++ api/hello/index.js
@@ -1,6 +1,16 @@
-module.exports = async function (context, req) {
-  context.res = {
-      // status: 200, /* Defaults to 200 */
-      body: "Hello from the API"
-  };
-}
+import express from "express";
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import { createHandler } from 'azure-function-express';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+
+app.get("/api/hello", (req, res) => {
+  res.status(200).send("Hello from DALL-E");
+});
+
+export default createHandler(app);
