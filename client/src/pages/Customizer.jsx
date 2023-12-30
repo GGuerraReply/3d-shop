d22c2017351dacc971625740832627f1b49fd533
GGuerraReply <g.guerra@reply.it>
2023-12-30 13:53:12 +0100

    Mobile fix

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -135,7 +135,7 @@ const Customizer = () => {
             className='absolute top-0 left-0 z-10'
             {...slideAnimation("left")}
           >
-            <div className='flex items-center min-h-screen'>
+            <div className='flex items-center h-dvh'>
               <div className='editortabs-container tabs'>
                 {EditorTabs.map((tab, index) => (
                   <Tab 
